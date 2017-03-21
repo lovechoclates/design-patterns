38ac0207641ec71e6e2a151c19c10b1ab8814b75
hadarbmnielsen <hadar.maor@nielsen.com>
2017-03-21 13:45:38 +0200

    make "accept" public

--- src/patterns/visitor/structure/IntExp.java
+++ src/patterns/visitor/structure/IntExp.java
@@ -3,12 +3,12 @@ package patterns.visitor.structure;
 import patterns.visitor.structure.tools.ExpressionVisitor;
 
 /**
- * Created by hadar on 21/03/2017.
+ * Integer expression representation
  */
 public class IntExp extends Expression {
     public int value;
 
-    protected void accept(ExpressionVisitor v) {
+    public void accept(ExpressionVisitor v) {
         v.visitIntExpression(this);
     }
 }
